30595f451991d956a6b3a4b9efa5b7e4cb1b8897
jarzuaga <jose.arzuaga@kaplan.com>
2013-06-12 20:22:28 -0400

    Initial checkin for Jun13

---
+++ Jun13/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Jun13
+//
+//  Created by jarzuaga on 6/12/13.
+//  Copyright (c) 2013 jarzuaga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
