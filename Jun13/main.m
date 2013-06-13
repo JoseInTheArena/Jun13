eb95c548c654a88fa71acba6b01885a4d3d03e09
jarzuaga <jose.arzuaga@kaplan.com>
2013-06-12 20:30:29 -0400

    Added homework code to comply with "Insert some statements that create variables and call the function NSLog like the examples in objective.html and Project. Insert the new statements immediately before the return statement in the main function in the main.m file in the Supporting Files folder of your project."

--- Jun13/main.m
+++ Jun13/main.m
@@ -13,6 +13,19 @@
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
+
+        for(int i = 1; i<=10; i++){
+            NSLog(@"The value of i is %d", i);
+        }
+        int integer = 100;
+        NSLog(@"%d is an int variable", integer);
+        
+        CGFloat floating = 2.71828;
+        NSLog(@"%g is a floating point variable", floating);
+        
+        NSString *s = @"This is a string.";
+        NSLog(@"The address of s is %p.", s);
+        
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
